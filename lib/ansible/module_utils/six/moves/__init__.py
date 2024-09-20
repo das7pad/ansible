8546e56de9b2a9716aa28f4d31e1fc866af6bafb
Jakob Ackermann <das7pad@outlook.com>
2024-09-20 18:43:36 +0200

    [misc] provide more "real" ansible.module_utils.six.moves packages

--- lib/ansible/module_utils/six/moves/__init__.py
+++ lib/ansible/module_utils/six/moves/__init__.py
@@ -4,15 +4,15 @@ from ansible.module_utils.six import *
 __DEBUG = False
 
 for key, value in _importer.known_modules.items():
-    if key == 'ansible.module_utils.six.moves':
+    if key == __name__:
         for key2 in dir(value):
             try:
                 locals()[key2] = getattr(value, key2)
                 if __DEBUG: print(key2, locals()[key2])
             except:
                 if __DEBUG: print(key2)
-    if key.find('ansible.module_utils.six.moves.') != 0: continue
-    key = key.replace('ansible.module_utils.six.moves.', '')
+    if key.find(__name__ + '.') != 0: continue
+    key = key.replace(__name__ + '.', '')
     try:
         locals()[key] = value._resolve()
         if __DEBUG: print(key, locals()[key])
